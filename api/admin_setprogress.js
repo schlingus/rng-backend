aa81b5aaf24b5c0367fefb16158ed0edb0936661
schlingus <163059875+schlingus@users.noreply.github.com>
2025-05-29 02:43:04 +0000

    yes

--- api/admin_setprogress.js
+++ api/admin_setprogress.js
@@ -6,7 +6,7 @@ const redis = new Redis({
 });
 
 // For security, consider moving this to an environment variable instead of hardcoding!
-const ADMIN_PASS = 'iHATEpickles1';
+const ADMIN_PASS = 'andrew s. is short';
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
